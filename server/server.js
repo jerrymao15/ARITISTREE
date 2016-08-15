e4486662f609a6bc614f9a35e6d5ae8db5d4054b
jerrymao15 <jerryy.mao@gmail.com>
2016-08-14 23:11:43 -0700

    redid server code to use promises for async

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
 'use strict';
-
 const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
 const request = require('request');
-const server = app.listen(3000, () => {console.log('listening on 3000...');});
-let norepeats = [];
 
+const PORT = 3000;
+const DEVPORT = 9090;
+const artistInfo = {};
+const norepeats = [];
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}.`));
 app.use(cors());
 app.use(express.static(path.join(__dirname, './../')));
 
@@ -16,45 +19,75 @@ const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
 const config = require('../webpack.config');
 
+function getRelated(id) {
+  console.log(id);
+  return new Promise((resolve, reject) => {
+    request(`https://api.spotify.com/v1/artists/${id}/related-artists`, (err, res, html) => resolve(JSON.parse(html).artists.map(ele => ele.name)));
+  });
+}
+
+function formatData(arr) {
+  const related = [];
+  for (let i = 0; related.length < 3 && i < arr.length; i++) {
+    const current = arr[i];
+    if (norepeats.indexOf(current) === -1) {
+      related.push({ artist: current, children: [] });
+      norepeats.push(current);
+    }
+  }
+  return JSON.stringify(related);
+}
+
 app.get('/artist/:artist', (req, res) => {
-  if (norepeats.length === 0) norepeats.push(req.params.artist);
-  request(`https://api.spotify.com/v1/search?q=${req.params.artist}&type=artist&limit=1`, (err, response, html) => {
-    const artistID = JSON.parse(html).artists.items[0].id;
-    request(`https://api.spotify.com/v1/artists/${artistID}/related-artists?q=limit=3`, (err2, response2, html2) => {
-      const related = [];
-      for (let i = 0; related.length < 3 && JSON.parse(html2).artists[i]; i++) {
-        let musicppl = JSON.parse(html2).artists[i].name;
-        if (norepeats.indexOf(musicppl)=== -1) {
-          related.push({artist: musicppl, children: []});
-          norepeats.push(musicppl)
-        }
-      }
-      res.send(JSON.stringify(related))
-      res.end();
+  const currentArtist = req.params.artist;
+  if (norepeats.length === 0) norepeats.push(currentArtist);
+  if (!artistInfo[currentArtist]) {
+    artistInfo[currentArtist] = {};
+    new Promise((resolve, reject) => {
+      request(`https://api.spotify.com/v1/search?q=${currentArtist}&type=artist&limit=1`, (err, response, html) => resolve(html));
     })
-  })
-})
+    .then(info => {
+      const spotifyInfo = JSON.parse(info).artists.items[0];
+      artistInfo[currentArtist].id = spotifyInfo.id;
+      artistInfo[currentArtist].imageURL = spotifyInfo.images[0].url;
+      return getRelated(artistInfo[currentArtist].id);
+    }, rej => { throw new Error(`Cannot find aritst ${currentArtist}!`); })
+    .then(fufill => {
+      artistInfo[currentArtist].related = fufill;
+      res.send(formatData(fufill));
+      console.log(artistInfo);
+      res.end();
+    });
+  } else {
+    res.send(formatData(artistInfo[currentArtist.related]));
+    res.end();
+  }
+});
+
+// app.get('info/:artist', (req, res) => {
+//   request(`http://api.spotify.com/v1/search/${}/`)
+// })
 
 app.get('/app.js', (req, res) => {
   if (process.env.PRODUCTION) {
-    res.sendFile(__dirname + '/client/app.js');
+    res.sendFile(path.join(__dirname, '/client/app.js'));
   } else {
-    res.redirect('//localhost:9090/client/app.js');
+    res.redirect(`//localhost:${DEVPORT}/client/app.js`);
   }
 });
 
 // Serve aggregate stylesheet depending on environment
 app.get('/style.css', (req, res) => {
   if (process.env.PRODUCTION) {
-    res.sendFile(__dirname + '/client/style.css');
+    res.sendFile(path.join(__dirname, '/client/style.css'));
   } else {
-    res.redirect('//localhost:9090/client/style.css');
+    res.redirect(`//localhost:${DEVPORT}/client/style.css`);
   }
 });
 
 // Serve index page
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../', 'index.html'))
+  res.sendFile(path.join(__dirname, '../', 'index.html'));
   // res.sendFile(__dirname + '/index.html');
 });
 
@@ -62,10 +95,8 @@ new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   noInfo: true,
-  historyApiFallback: true
-}).listen(9090, 'localhost', (err, result) => {
-  if (err) {
-    console.log(err);
-  }
+  historyApiFallback: true,
+}).listen(DEVPORT, 'localhost', (err, result) => {
+  if (err) console.log(err);
   console.log('webpack dev server started');
 });
