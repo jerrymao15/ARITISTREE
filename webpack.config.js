52d484b6a845f175b2fa5cf991715415b7c50b8c
jerrymao15 <jerryy.mao@gmail.com>
2016-08-15 00:34:09 -0700

    edited bundle paths

--- webpack.config.js
+++ webpack.config.js
@@ -12,9 +12,9 @@ module.exports = {
   // This will not actually create a bundle.js file in ./client. It is used
   // by the dev server for dynamic hot loading.
   output: {
-    path: __dirname + '/client/',
+    path: __dirname + '/src/build/',
     filename: 'app.js',
-    publicPath: 'http://localhost:9090/client/',
+    publicPath: 'http://localhost:9090/src/build/',
   },
   resolve: {
     extensions: ['', '.js', '.jsx'],
